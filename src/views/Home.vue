de1433d619eed9915a51da2e9a398e1ebb3e3d2c
Arvic Kenedi <arvic2514@gmail.com>
2025-06-06 15:04:12 -0500

    feat: initialize Vue 3 project with Vite and Tailwind CSS
    - Added package.json with dependencies for Vue, Vue Router, Vite, and Tailwind CSS.
    - Created postcss.config.js for PostCSS configuration.
    - Added SVG assets for Vite and Vue logos.
    - Implemented main application structure in App.vue.
    - Created HelloWorld component with basic interactivity.
    - Set up Tailwind CSS with input.css and output.css.
    - Configured main.js to initialize Vue app with router.
    - Established routing in router/index.js with Home and HelloWorld routes.
    - Developed Home.vue view with a welcoming message.
    - Configured Tailwind CSS in tailwind.config.js.
    - Set up Vite configuration in vite.config.js with path aliasing.

---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="home">
+        <h1>Home</h1>
+        <p>Welcome to your new Dev portafolio - Arvic! aun en construción</p>
+        <span style="font-family: Arial, sans-serif; font-size: 100px;">🥺</span>
+    </div>
+</template>
+
+<style scoped>
+
+.home {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    text-align: center;
+}
+
+</style>
